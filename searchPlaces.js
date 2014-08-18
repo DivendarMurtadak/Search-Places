1774239a21c2aabd2c144a29a1a698e9afaff3c4
divendar murtadak <divendar.murtadak@gmail.com>
2014-08-18 04:18:23 -0500

    Update searchPlaces.js

--- searchPlaces.js
+++ searchPlaces.js
@@ -1,60 +1,67 @@
 var geocoder;
 var map;
 var markers = [];
-  var strLatLng;
-  var southWest ;
-  var northEast;
-  var bounds ;
-  
-  document.write('<');
-  document.write('script ');
-  document.write('src="');
-  //next line is the path to jquery file
-  document.write('/javascripts/jquery-2.1.1.min.js');
-  document.write('" type="text/javascript"></');
-  document.write('script');
-  document.write('>');
-  // to set initial Map boundry 
-function initialize()
-{
+var strLatLng;
+var southWest;
+var northEast;
+var bounds;
 
-	this.lat=32.75;
-	this.lng=-97.13;
-	geocoder = new google.maps.Geocoder();
-this.myCenter=new google.maps.LatLng(this.lat,this.lng);
-this.mapProp = {
-  center:myCenter,
-  zoom:16,
-  mapTypeId:google.maps.MapTypeId.ROADMAP
-  };
+document.write('<');
+document.write('script ');
+document.write('src="');
+//next line is the path to jquery file
+document.write('/javascripts/jquery-2.1.1.min.js');
+document.write('" type="text/javascript"></');
+document.write('script');
+document.write('>');
+function initialize() {
+	var mapOptions = {
+		zoom : 16
+	};
+	map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
- map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
- 
-}
+	// Try HTML5 geolocation
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var pos = new google.maps.LatLng(position.coords.latitude,
+					position.coords.longitude);
 
-// add markers on click of map location
-function addMarker(latitude,longitude) {
-	
-	 var map = new google.maps.Map(document.getElementById('googleMap'), { 
-	     mapTypeId: google.maps.MapTypeId.TERRAIN
-	   });
+			var infowindow = new google.maps.InfoWindow({
+				map : map,
+				position : pos,
+				content : 'Your current Location.'
+			});
 
-	   var markerBounds = new google.maps.LatLngBounds();
-
-	   var randomPoint, i;
-
-	     randomPoint = new google.maps.LatLng(latitude,longitude);
-	     new google.maps.Marker({
-	       position: randomPoint, 
-	       map: map
-	     });
-
-	     markerBounds.extend(randomPoint);
-	 	   map.fitBounds(markerBounds);
-}
-
-
-function callDB(name, type, icon, latLang)
-{
-	
+			map.setCenter(pos);
+		}, function() {
+			handleNoGeolocation(true);
+		});
+	} else {
+		// Browser doesn't support Geolocation
+		handleNoGeolocation(false);
 	}
+}
+
+function addMarker(latitude, longitude) {
+
+	var map = new google.maps.Map(document.getElementById('googleMap'), {
+		mapTypeId : google.maps.MapTypeId.TERRAIN
+	});
+
+	var markerBounds = new google.maps.LatLngBounds();
+
+	var randomPoint, i;
+
+	randomPoint = new google.maps.LatLng(latitude, longitude);
+	new google.maps.Marker({
+		position : randomPoint,
+		map : map
+	});
+
+	markerBounds.extend(randomPoint);
+	map.fitBounds(markerBounds);
+}
+
+function callDB(name, type, icon, latLang) {
+
+}
