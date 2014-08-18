7e84d8c3d92a495d824a829bf293f4137308fa31
divendar murtadak <divendar.murtadak@gmail.com>
2014-08-18 04:16:49 -0500

    Update bookmarked.php

--- bookmarked.php
+++ bookmarked.php
@@ -1,45 +1,57 @@
 <?php
-echo '<html><head><title>Googe Place API Bookmarked</title>'; 
-echo '<link  href=style.css rel="stylesheet"></head>';
+echo '<html><head><title>Googe Place API Bookmarked</title>';
+echo '<link  href=style.css rel="stylesheet">';
+echo '<link  href="bootstrap/css/bootstrap-responsive.css" rel="stylesheet">';
+echo '<link  href="bootstrap/css/bootstrap.css" rel="stylesheet">';
+// echo ' <script type="text/javascript" src="jquery-2.1.1.min.js"></script>';
+echo ' <script type="text/javascript" src="searchPlaces.js"></script>';
+echo ' <script type="text/javascript" src="bootstrap/js/bootstrap.js"></script>';
+
+echo ' <script type="text/javascript" src="bootstrap/js/bootstrap.min.js"></script></head>';
 require_once 'DB_Functions.php';
-$db = new DB_Functions();
+$db = new DB_Functions ();
+$con = mysql_connect ( 'omega.uta.edu', 'test', 'test' );
 
-$con = mysql_connect('localhost', 'root', 'deven143@deven');
-
-#Check connection
-if (mysql_errno()) {
-echo 'Database connection error: ' . mysql_error();
-exit();
+// heck connection
+if (mysql_errno ()) {
+	echo 'Database connection error: ' . mysql_error ();
+	exit ();
 }
 
-mysql_select_db('places');
+mysql_select_db ( 'ajm6025' );
 
-$results = mysql_query("SELECT * from places_search")  or die(mysql_error());
-
+$results = mysql_query ( "SELECT * from places_search" ) or die ( mysql_error () );
 function mysql_fetch_all($res) {
-	while($row=mysql_fetch_array($res)) {
-		$return[] = $row;
+	while ( $row = mysql_fetch_array ( $res ) ) {
+		$return [] = $row;
 	}
 	return $return;
 }
-
 function create_table($dataArr) {
 	echo "<tr>";
-	for($j = 0; $j < count($dataArr); $j++) {
-		if (isset($dataArr[$j]))
-		echo "<td>".$dataArr[$j]."</td>";
+	for($j = 0; $j < count ( $dataArr ); $j ++) {
+		if (isset ( $dataArr [$j] )) {
+			if ($j == 3) {
+				echo "<td><img class='img-circle' src=" . $dataArr [$j] . "/></td>";//
+			} else {
+				echo "<td>" . $dataArr [$j] . "</td>";
+			}
+		}
 	}
 	echo "</tr>";
 }
 
-$all = mysql_fetch_all($results);
+$all = mysql_fetch_all ( $results );
 
-echo "<table class='data_table'>";
+echo '<table class="table table-condensed">';
+echo '<tr><th>Sr No.</th><th> Name</th><th>Type</th><th>Icon</th><th>Location</th></tr>';
 
-for($i = 0; $i < count($all); $i++) {
-	create_table($all[$i]);
+for($i = 0; $i < count ( $all ); $i ++) {
+	create_table ( $all [$i] );
 }
 
 echo "</table>";
-mysql_close();
+
+echo '<a class="btn btn-large btn-primary" type="button" href="searchPlaces.php">Return to Search</a>';
+mysql_close ();
 ?>
