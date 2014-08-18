6f94d43e57a3d9322823fd0e623b1a357d3400fd
divendar murtadak <divendar.murtadak@gmail.com>
2014-08-18 04:17:46 -0500

    Update DB_Functions.php

--- DB_Functions.php
+++ DB_Functions.php
@@ -1,31 +1,25 @@
 <?php
- 
 class DB_Functions {
- 
-    private $db;
- 
-    public function storeBookmarks($name,$type,$icon,$latLng)
-    {
-    	// setup query
-    	$q = "INSERT INTO `places_search` (`place_name`, `place_type`, `place_icon`, `place_latLng`) VALUES('$name', '$type', '$icon', '$latLng')";
-    	
-    	//Run Query
-    	$result = mysql_query($q) or die(mysql_error());
-        }
-    
-    public function getBookmarks() {
-    	        $result = mysql_query("SELECT * from places_search")  or die(mysql_error());
-    
-    	        // check for result
-    	        $no_of_rows = mysql_num_rows($result);
-    	        if ($no_of_rows > 0) {
-    	           // $result = mysql_fetch_array($result);
-    	            return $result;
-    	        } else {
-    	             return false;
-    	        }
-    	    }
-    	    
-    	   }
- 
+	private $db;
+	public function storeBookmarks($name, $type, $icon, $latLng) {
+		// setup query
+		$q = "INSERT INTO `places_search` (`place_name`, `place_type`, `place_icon`, `place_latLng`) VALUES('$name', '$type', '$icon', '$latLng')";
+		
+		// Run Query
+		$result = mysql_query ( $q ) or die ( mysql_error () );
+	}
+	public function getBookmarks() {
+		$result = mysql_query ( "SELECT * from places_search" ) or die ( mysql_error () );
+		
+		// check for result
+		$no_of_rows = mysql_num_rows ( $result );
+		if ($no_of_rows > 0) {
+			// $result = mysql_fetch_array($result);
+			return $result;
+		} else {
+			return false;
+		}
+	}
+}
+
 ?>
